7acb84175510685d38214bdd9d36ddd614267e3a
Juank Prada <881798+juankprada@users.noreply.github.com>
2023-08-10 18:09:22 +0900

    Had to rely on an Bash script to 'automate' the run of ansible, which is stupid but meh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+echo ">>> Setting up my awesome environment"
+
+ansible-playbook ssh-setup-playbook.yml --ask-vault-pass
+
+eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_ed25519
+
+ansible-playbook awesome-setup.yml --ask-become-pass
