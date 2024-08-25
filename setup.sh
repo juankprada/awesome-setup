a90e3f004b8c9bd9ae8e63e9f46973c7c9bbcff6
Juan Camilo Prada <juankprada@gmail.com>
2024-08-25 23:28:18 +0900

    Refactors awesome setup to integrate better tooling and Dev env setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,100 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+
+set -e
+
+
+# Checks if gum is installed
+check_gum() {
+    if command -v gum -v >/dev/null 2>&1; then
+	return 0
+    else
+	return 1
+    fi
+}
+
+install_gum_macos() {
+    if ! command -v brew >/dev/null 2>&1; then
+	echo "Hombre is not installed. Installing Homebrew first..."
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+    brew install gum
+}
+
+
+# Function to install Ruby on Ubuntu using apt-get
+install_gum_ubuntu() {
+    sudo apt-get update > /dev/null
+    sudo apt-get install -y gum >/dev/null
+    
+}
+
+install_gum_fedora() {
+    sudo dnf install -y gum >/dev/null
+}
+
+
+detect_linux_de() {
+    export RUNNING_GNOME=$([[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]] && echo true || echo false)
+    export RUNNING_KDE=false
+}
+		 
+
+OS=$(uname)
+if [ "$OS" = "Darwin" ]; then
+    OS="Mac"
+    install_gum_macos
+elif [ "$OS" = "Linux" ]; then
+    detect_linux_de
+    if [ -f /etc/os-release ]; then
+	. /etc/os-release
+	case "$ID" in
+	    ubuntu)
+		OS="Ubuntu"
+		install_gum_ubuntu
+	    ;;
+	    fedora)
+		OS="Fedora"
+		install_gum_fedora
+	    ;;
+	    *)
+		echo "Unsupported distribution"
+		exit 1
+		;;
+	esac
+    else
+	echo "Unsupported OS."
+	exit 1
+    fi
+fi
+
+ascii_art='   ______           __
+  / ____/___ ______/ /_  ___ _      __
+ / /   / __ `/ ___/ __ \/ _ \ | /| / /
+/ /___/ /_/ (__  ) / / /  __/ |/ |/ /
+\____/\__,_/____/_/ /_/\___/|__/|__/
+
+'
+
+
+
+
+
+# Make sure we have ansible collections
+ansible-galaxy collection install community.general
+
+
+
+
+# Default Selections
+export INSTALL_JAVA_ENV=true
+export INSTALL_RUBY_ENV=true
+
 
 # ask for sudo pass
-read -s -p "SUDO password: " password
+
+gum input --password --value "$password"
+#read -s -p "SUDO password: " password
 
 export ANSIBLE_BECOME_PASSWORD=$password
 
