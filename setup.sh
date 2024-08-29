8a96f14cea32356b866df021f34eb19ed1f83334
Juan Camilo Prada <juankprada@gmail.com>
2024-08-29 17:23:36 +0900

    Adds Gum installation for fedora

--- setup.sh
+++ setup.sh
@@ -30,6 +30,14 @@ install_gum_ubuntu() {
 }
 
 install_gum_fedora() {
+    echo '[charm]
+name=Charm
+baseurl=https://repo.charm.sh/yum/
+enabled=1
+gpgcheck=1
+gpgkey=https://repo.charm.sh/yum/gpg.key' | sudo tee /etc/yum.repos.d/charm.repo
+    sudo rpm --import https://repo.charm.sh/yum/gpg.key
+    
     sudo dnf install -y gum >/dev/null
 }
 
